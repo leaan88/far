cb40c087c7642ae6eb58ce4cfde60f34cf228075
Leandro <83724929+leaan88@users.noreply.github.com>
2025-02-20 05:05:06 -0300

    Create NotFound.jsx

---
+++ project1/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>404 - Página no encontrada</h2>
+      <p>Lo sentimos, la dirección a la que intentas acceder no existe.</p>
+    </div>
+  );
+}
